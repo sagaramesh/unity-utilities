ac9569597f2366041e74438bf22352362d6cbc31
Sagar Ramesh <hey@sagarramesh.com>
2022-11-30 22:22:27 -0800

    Getting started...

---
+++ Scripts/FadeMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach this to a GameObject with a material (standard shader, fade rendering mode) you want to fade in / out.
+public class FadeMaterial : MonoBehaviour
+{
+    [SerializeField] private float _transitionTime = 0.5f;
+    private Color _transparent = new Color(0, 0, 0, 0);
+
+    public void Start()
+    {
+        // Fade from current color to transparent.
+        StartCoroutine(Fade(_transparent, _transitionTime));
+    }
+
+    IEnumerator Fade(Color endValue, float duration)
+    {
+        float time = 0;
+        Material thisMaterial = this.GetComponent<MeshRenderer>().material;
+        Color startValue = thisMaterial.color;
+
+        while (time < duration)
+        {
+            thisMaterial.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        thisMaterial.color = endValue;
+    }
+}
---
+++ Scripts/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour{
+
+    public bool sampleBool;
+    private static Singleton _instance;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+            _instance = GameObject.FindObjectOfType<Singleton>();
+
+            if (_instance == null)
+            {
+                GameObject go = new GameObject(nameof(Singleton));
+                _instance = go.AddComponent<Singleton>();
+            }
+            return _instance;
+        }
+    }
+
+    void SampleUsage() {
+        Singleton.Instance.sampleBool = true;
+    }
+}
